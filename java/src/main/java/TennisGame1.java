aebb42b444280a8cfa91827984114d0a9c8ceb52
Debo790 <andreadebeni95@gmail.com>
2023-12-01 15:02:20 +0100

    first part

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,59 +1,39 @@
+import java.util.Map;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
+    private final Map<Integer, String> pointToScoreString = Map.of(0, "Love-All", 1, "Fifteen-All", 2, "Thirty-All");
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1() {
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (scorePlayer1 == scorePlayer2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = pointToScoreString.getOrDefault(scorePlayer1, "Deuce");
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scorePlayer1 >=4 || scorePlayer2 >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+
+            score = calculateScore(scorePlayer1, scorePlayer2);
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = scorePlayer1;
+                else { score+="-"; tempScore = scorePlayer2;}
                 switch(tempScore)
                 {
                     case 0:
@@ -72,5 +52,20 @@ public class TennisGame1 implements TennisGame {
             }
         }
         return score;
+    }
+
+    private static String calculateScore(int scorePlayer1, int scorePlayer2) {
+        int scoreDifference = scorePlayer1 - scorePlayer2;
+
+        if (scoreDifference == 1) {
+            return "Advantage player1";
+        } else if (scoreDifference == -1) {
+            return "Advantage player2";
+        } else if (scoreDifference >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+
     }
 }
