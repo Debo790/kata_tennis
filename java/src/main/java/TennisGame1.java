ef0c57c37c2b32e0d92c1931c19dc0e00d69048e
Marco <marco@wallstreetfootball.io>
2023-12-01 15:31:55 +0100

    refactor tennisgame1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,62 +1,39 @@
 import java.util.Map;
 
 public class TennisGame1 implements TennisGame {
+
+    private static final String scoreTemplate = "%s-%s";
+    private static final Map<Integer, String> deucesToScoreString = Map.of(0, "Love-All", 1, "Fifteen-All", 2, "Thirty-All");
+    private static final Map<Integer, String> pointToScoreString = Map.of(0, "Love", 1, "Fifteen", 2, "Thirty", 3, "Forty");
     
     private int scorePlayer1 = 0;
     private int scorePlayer2 = 0;
-    private final Map<Integer, String> pointToScoreString = Map.of(0, "Love-All", 1, "Fifteen-All", 2, "Thirty-All");
 
     public TennisGame1() {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if ("player1".equals(playerName)) {
             scorePlayer1 += 1;
-        else
+        } else {
             scorePlayer2 += 1;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (scorePlayer1 == scorePlayer2)
-        {
-            score = pointToScoreString.getOrDefault(scorePlayer1, "Deuce");
+        if (scorePlayer1 == scorePlayer2) {
+            return deucesToScoreString.getOrDefault(scorePlayer1, "Deuce");
+        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
+            return calculateAdvantagesOrWin(scorePlayer1, scorePlayer2);
+        } else {
+            String scorePlayer1Str = pointToScoreString.get(this.scorePlayer1);
+            String scorePlayer2Str = pointToScoreString.get(this.scorePlayer2);
+            return scoreTemplate.formatted(scorePlayer1Str, scorePlayer2Str);
         }
-        else if (scorePlayer1 >=4 || scorePlayer2 >=4)
-        {
-
-            score = calculateScore(scorePlayer1, scorePlayer2);
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = scorePlayer1;
-                else { score+="-"; tempScore = scorePlayer2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
 
-    private static String calculateScore(int scorePlayer1, int scorePlayer2) {
+    private static String calculateAdvantagesOrWin(int scorePlayer1, int scorePlayer2) {
         int scoreDifference = scorePlayer1 - scorePlayer2;
-
         if (scoreDifference == 1) {
             return "Advantage player1";
         } else if (scoreDifference == -1) {
@@ -66,6 +43,5 @@ public class TennisGame1 implements TennisGame {
         } else {
             return "Win for player2";
         }
-
     }
 }
